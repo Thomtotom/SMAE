ef0000de5cabedffb7f265a7bd6454b00d7bcbdc
t-leith <leithtorrens@gmail.com>
2020-08-05 16:32:23 +1200

    first commit

---
+++ CODE/index.js
@@ -0,0 +1,3 @@
+function show_alert() {
+    alert("THE PEOPLE OF SMAE NEED YOU! join the third army now!<br >the lord of ____ has just pushed threw the seventh wall leaving the sixthed circle helpless");
+}
